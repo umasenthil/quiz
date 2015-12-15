bde084b92714ff653c5a6bc409c79336d353db15
umasenthil <uma_senthil@hotmail.com>
2015-12-15 15:06:51 +0000

    Added color and age column to cats table

---
+++ db/migrate/20151215145535_add_color_column_to_cats.rb
@@ -0,0 +1,6 @@
+class AddColorColumnToCats < ActiveRecord::Migration
+  def change
+    add_column :cats, :color, :string 
+    add_column :cats, :age, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215143320) do
+ActiveRecord::Schema.define(version: 20151215145535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151215143320) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
+    t.integer  "age"
   end
 
 end
